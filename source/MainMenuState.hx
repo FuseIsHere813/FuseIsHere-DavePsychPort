c242faf328dccf54f628585340d994881bf0ad36
FuseIsHere813 <77396398+FuseIsHere813@users.noreply.github.com>
2021-12-14 23:28:01 -0500

    Beta 1

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -25,7 +25,7 @@ using StringTools;
 class MainMenuState extends MusicBeatState
 {
 	public static var psychEngineVersion:String = '0.4.2'; //This is also used for Discord RPC
-	public static var PortVersion:String = '1.0';
+	public static var PortVersion:String = 'Beta 1 (Testing Purposes Only)';
 	public static var curSelected:Int = 0;
 
 	var menuItems:FlxTypedGroup<FlxSprite>;
